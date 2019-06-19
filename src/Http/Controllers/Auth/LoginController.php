b06afaa2a3026d2e1fe534de15dfa342fdc7cd3b
Ivan Ivanov <zugurysus@gmail.com>
2019-06-19 11:08:46 +1000

    mis fix

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace reactmay\WoWAuth\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Lang;
