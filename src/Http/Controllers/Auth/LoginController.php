6e5ab40625af55e343a4dba867885b490ce039f4
Ivan Ivanov <zugurysus@gmail.com>
2019-06-19 15:51:20 +1000

    Auth success message еще р cancel

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -29,12 +29,6 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    protected function authenticated(Request $request, $user){
-        $successmessage = 'Hej '.$this->username().', you have been successfully logged in!';
-        $request->session(['message' => $successmessage]);
-        return redirect()->intended($this->redirectPath());
-    }
-
 	protected $username;
 	
     /**
