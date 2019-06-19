9d1d2d5fe5e8b3143e816daba67ed22fec2ea713
Ivan Ivanov <zugurysus@gmail.com>
2019-06-19 12:08:44 +1000

    mis fix validate 1

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace reactmay\WoWAuth\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Validator;
 use Lang;
 
 /**
